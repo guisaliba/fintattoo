a6ff0822392c8277c29c092c675c41db581fc52d
Guilherme Saliba <salibagui19@gmail.com>
2025-04-14 00:09:15 -0300

    chore: update AppService root route

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return homepage welcoming message', () => {
+      expect(appController.getHomepage()).toBe(
+        'Welcome to Fintattoo homepage!',
+      );
     });
   });
 });
