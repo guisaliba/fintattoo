4fbfad8be132c52dc6f0d6b7d5b78fefae6800f2
Gabriel Bonfim <gabriel.m.m.bonfim@gmail.com>
2025-05-03 20:00:17 -0300

    chore: remove debug logs

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,6 @@
 import { DataSource } from 'typeorm';
 import 'dotenv/config';
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USERNAME:', process.env.DB_USERNAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
