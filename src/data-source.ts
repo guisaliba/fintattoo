532c1eb3cd0da9b5eb83fb37f3c371255b8423bd
Guilherme Saliba <salibagui19@gmail.com>
2025-05-03 19:39:04 -0300

    style: change quote_type to single quote

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,14 @@
-import { DataSource } from "typeorm";
-import "dotenv/config";
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USERNAME:", process.env.DB_USERNAME);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", process.env.DB_NAME);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432, // default port or throw error
   username: process.env.DB_USERNAME,
@@ -16,6 +16,6 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
   entities: [],
-  migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   synchronize: false,
 });
